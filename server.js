1fa7902b0191508ba39163feb20d9c97125fb20c
walt24 <wandujar24@gmail.com>
2017-07-25 23:35:12 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-
 const express = require('express');
 const router = express.Router();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const {ShoppingList} = require('./models');
+const {Recipes} = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -18,11 +18,18 @@ ShoppingList.create('beans', 2);
 ShoppingList.create('tomatoes', 3);
 ShoppingList.create('peppers', 4);
 
+Recipes.create('cake',['flour', 'egg','vanilla extract'])
+Recipes.create('cuba libre',['rum','coke','lemon'])
+
 // when the root of this router is called with GET, return
 // all current ShoppingList items
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
 });
+
+app.get('/recipes',function(req,res){
+	res.json(Recipes.get());
+})
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
